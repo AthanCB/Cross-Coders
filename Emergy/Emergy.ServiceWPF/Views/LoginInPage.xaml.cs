91c0acf9901f67f5162aca7d1d5d855158282e33
Georg Dimopoulos <georgdimopoulos@gmail.com>
2017-05-12 18:57:03 +0300

    Server: Log In Window

---
+++ Emergy/Emergy.ServiceWPF/Views/LoginInPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace Emergy.ServiceWPF.Views
+{
+    /// <summary>
+    /// Interaction logic for LoginInPage.xaml
+    /// </summary>
+    public partial class LoginInPage : Window
+    {
+        public LoginInPage()
+        {
+            InitializeComponent();
+        }
+
+	    private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+	    {
+		    this.Hide();
+	    }
+    }
+}
