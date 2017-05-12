36fb92279ca3a31e4fec976122b79ca6577db096
AthanCB <athancb@hotmail.com>
2017-05-12 16:22:55 +0300

    Service: Folders

---
+++ Emergy/Emergy.ServiceWPF/Models/Signal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xaml.Schema;
+
+namespace Emergy.ServiceWPF.Models
+{
+    class Signal
+    {
+        private string _id;
+    }
+}
